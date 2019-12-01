2197a24f6c5bf4218250a738d69e4e388d717d86
SungHyun Yu <ysh1513@naver.com>
2019-12-01 14:40:31 +0900

    회원로그인->회원가입 퍼그

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('login',{});
+  res.render('login');
+});
+router.get('/join', function(req, res, next) {
+  res.render('join');
 });
 
 module.exports = router;
