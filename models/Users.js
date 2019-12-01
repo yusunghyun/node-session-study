d6d6b28dc51772053243f79434a76cc6818f678d
SungHyun Yu <ysh1513@naver.com>
2019-12-01 14:58:30 +0900

    mysql에 연동

---
+++ models/Users.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, Sequelize) => {
+  return sequelize.define(
+    "members",
+    {
+      userid: {
+        type: Sequelize.STRING(16),
+        allowNull: false,
+        unique: true //고유값
+      },
+      userpw: {
+        type: Sequelize.STRING(255),
+        allowNull: false //기본값 true  = //null허용
+      },
+      username: {
+        type: Sequelize.STRING(255)
+      }
+    },
+    {
+      timestamps: true
+    }
+  );
+};
